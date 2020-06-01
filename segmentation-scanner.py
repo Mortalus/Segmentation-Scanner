23b80257abee01ab62e9112991b41960cf9112cb
jeffreyvonrotz <jeffreyrotz@gmail.com>
2020-06-01 05:10:50 -0400

    All 3 command work in succession

--- segmentation-scanner.py
+++ segmentation-scanner.py
@@ -4,6 +4,7 @@ import sys, getopt
 def main(argv):
     target_vlan = ''
     target_range = ''
+
     try:
         opts, args = getopt.getopt(argv,"hv:t:",["vfile=","tfile="])
         #print(opts, args)
@@ -19,8 +20,17 @@ def main(argv):
         elif opt in ("-t", "--tfile"):
             target_range = arg
 
-    os.system("nmap -Pn -vvv -r -sS --reason --top-ports 1000 --max-retries 1 -oA nmap-sS-full." + target_vlan + " " + target_range)
-    #os.system("nmap -Pn -vvv -r -sU --top-ports 100 --reason --max-retries 0 -oA nmap-sU." + target_range + " " + target_range)
+    try:
+        os.mkdir(target_vlan)
+    except OSError:
+        print("Creation of the directory %s failed " % target_vlan)
+    else:
+        print("Successfully created the directory %s " % target_vlan)
+
+    os.system("nmap -Pn -vvv -r -sS --reason --top-ports 1000 --max-retries 1 -oA " + target_vlan + "/nmap-sS-full." + target_vlan + " " + target_range)
+    os.system("nmap -Pn -vvv -r -sU --top-ports 100 --reason --max-retries 0 -oA " + target_vlan + "/nmap-sU." + target_vlan + " " + target_range)
+    os.system("nmap -vvv -sO -p 0-2,4,6,17,41-44,47,50,51,58-60,89 -oA " + target_vlan + "/nmap-sO-" + target_vlan + " " + target_range)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
